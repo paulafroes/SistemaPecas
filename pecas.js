01902111bc30f34b7471dea58be6302832679dfa
Paula-froes <paula.froes@ymail.com>
2022-01-30 13:19:56 -0300

    Sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,26 @@
+let pesoPeca = 50
+
+if(pesoPeca > 100) {
+    console.log("Peso maior que 100g")
+} else {
+    console.log("Peso insuficiente, não é possivel cadastrar.")
+}
+
+let numeroPecas = 10
+
+if(numeroPecas < 10) {
+    console.log("Ainda há espaço na caixa, podemos cadastrar a peça")
+}else{
+    ("Não há espaço disponível na caixa")
+}
+
+let nomePeca = "Disco"
+
+console.log("O comprimento do nome da peça é:", nomePeca.length)
+
+if(nomePeca.length < 3) {
+    console.log("Nome de peça muito curto, não é possível cadastrar")
+} else {
+    console.log("nome de peça adequado, podemos cadastrar")
+}
+
